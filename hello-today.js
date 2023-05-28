313c3a41f5c7a3da2af8dfeac1cdac15fed12e44
Gregor Adams <1148334+pixelass@users.noreply.github.com>
2023-05-28 06:05:56 +0200

    Update hello-today.js

--- hello-today.js
+++ hello-today.js
@@ -2,6 +2,5 @@ export default function helloToday(name = "World") {
   const date = new Date();
   const day = date.getDay();
   const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-
   return `Hello ${name} today is ${days[day]}!`;
 }
